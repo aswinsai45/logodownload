d175ec5a4e1238bbe049fdebc5c2de731df0cf32
Sai Aswin Raja <aswinsai45@gmail.com>
2025-03-25 15:05:35 +0530

    Initialized components

---
+++ my-project/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div>
+      <div className=" flex items-center justify-center text-bold">
+        <h1>LogoWebsite</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ my-project/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="flex items-center justify-center font-bold box-border border-4">
+        <h1>LOGOWEBSITE</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
