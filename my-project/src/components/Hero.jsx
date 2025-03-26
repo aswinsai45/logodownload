264f34ad5ce2b6d803c80b5c2cc70a62a9c30f20
Sai Aswin Raja <aswinsai45@gmail.com>
2025-03-26 11:46:54 +0530

    fixed navbar and added searchbar

--- my-project/src/components/Hero.jsx
+++ my-project/src/components/Hero.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Hero = () => {
   return (
     <div>
-      <div className=" flex items-center justify-center text-bold">
-        <h1>LogoWebsite</h1>
+      <div className="flex items-center justify-center text-bold box-border p-5">
+        <div className="flex flex-row items-center justify-center box-border border-4 p-4 max-w-[600px] rounded-[20px]">
+          <FaSearch />
+          <input
+            className="px-5 outline-none"
+            type="text"
+            placeholder="search a sticker or logo"
+          />
+        </div>
       </div>
     </div>
   );
--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
     <div>
-      <div className="flex items-center justify-center font-bold box-border border-4">
-        <h1>LOGOWEBSITE</h1>
+      <div className="flex items-center justify-center p-5 bg-gray-200">
+        <h1 className="font-bold text-2xl">LOGOWEBSITE</h1>
       </div>
     </div>
   );
